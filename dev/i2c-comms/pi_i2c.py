bf2198418a114dcccc05f6c2a3e2fc46ed5e7ddb
Ishan Madan <19366470+ishanm0@users.noreply.github.com>
2023-10-05 23:53:39 -0700

    bidirectional i2c :)

--- dev/i2c-comms/pi_i2c.py
+++ dev/i2c-comms/pi_i2c.py
@@ -13,10 +13,17 @@ fcntl.ioctl(i2c_fd, I2C_PRIM, pico_address)
 
 # send data to pico
 data = [0x01, 0x02, 0x03]  # example data
-while (True):
-    os.write(i2c_fd, bytes(data))
-    time.sleep(1)
+while True:
+    try:
+        os.write(i2c_fd, bytes(data))
+        time.sleep(0.02)
+        print("Sent data to Pico: ", list(data))
+    except OSError:
+        print("Remote I/O Error")
     # read data from pico
-    incoming_data = os.read(i2c_fd, 3)  # read 3 bytes
-    time.sleep(1)
-    print("Received data from Pico: ", list(incoming_data))
+    try:
+        incoming_data = os.read(i2c_fd, 3)  # read 3 bytes
+        time.sleep(0.02)
+        print("Received data from Pico: ", list(incoming_data))
+    except TimeoutError:
+        print("Timeout Error")
--- dev/i2c-comms/pico_i2c.c
+++ dev/i2c-comms/pico_i2c.c
@@ -1,15 +1,63 @@
 #include <stdio.h>
-#include "pico/stdlib.h"
-#include "hardware/i2c.h"
+#include <pico/stdlib.h>
+#include <pico/i2c_slave.h>
+#include <hardware/i2c.h>
+
+#ifndef PICO_DEFAULT_LED_PIN
+#warning blink requires a board with a regular LED
+#else
+const uint LED_PIN = PICO_DEFAULT_LED_PIN;
+#endif
+
+uint8_t outgoing_data[3] = {0x11, 0x12, 0x13}; // example data
+uint8_t incoming_data[3];
+int data_index = 0;
+
+static void i2c_slave_handler(i2c_inst_t *i2c, i2c_slave_event_t event)
+{
+    switch (event)
+    {
+    case I2C_SLAVE_RECEIVE: // master has written some data
+        for (int i = 0; i < 3; i++)
+        {
+            if (incoming_data[i] == 0x00)
+            {
+                incoming_data[i] = i2c_read_byte_raw(i2c);
+                printf("Received data %d: 0x%02X\n ", i, incoming_data[i]);
+                gpio_put(LED_PIN, 1);
+                break;
+            }
+        }
+        break;
+    case I2C_SLAVE_REQUEST: // master is requesting data
+        i2c_write_byte_raw(i2c, outgoing_data[data_index]);
+        printf("Sent data %d: 0x%02X\n ", data_index, outgoing_data[data_index]);
+        gpio_put(LED_PIN, 0);
+        data_index++;
+        if (data_index > 2)
+        {
+            data_index = 0;
+        }
+        break;
+    case I2C_SLAVE_FINISH: // master has signalled Stop / Restart
+        data_index = 0;
+        for (int i = 0; i < 3; i++)
+        {
+            incoming_data[i] = 0x00;
+        }
+        break;
+    default:
+        break;
+    }
+}
 
 int main()
 {
     stdio_init_all();
 
 #ifndef PICO_DEFAULT_LED_PIN
-#warning blink example requires a board with a regular LED
+#warning blink requires a board with a regular LED
 #else
-    const uint LED_PIN = PICO_DEFAULT_LED_PIN;
     gpio_init(LED_PIN);
     gpio_set_dir(LED_PIN, GPIO_OUT);
 #endif
@@ -18,59 +66,12 @@ int main()
     i2c_init(i2c0, 100 * 1000);
     gpio_set_function(0, GPIO_FUNC_I2C);
     gpio_set_function(1, GPIO_FUNC_I2C);
-    gpio_pull_up(0);
-    gpio_pull_up(1);
-    // i2c_pullup_en(i2c0, true);
 
     // set i2c address for pico
-    i2c_set_slave_mode(i2c0, true, 0x08);
-    // i2c_set_slave_address(i2c0, 0x08); // address should match pi code
-
-    uint8_t outgoing_data[3] = {0x11, 0x12, 0x13}; // example data
-    uint8_t incoming_data[3];
+    i2c_slave_init(i2c0, 0x08, &i2c_slave_handler);
 
     while (1)
-    {
-        // reset incoming data
-        for (int i = 0; i < 3; i++)
-        {
-            incoming_data[i] = 0x00;
-        }
-
-        // if (i2c_get_read_available(i2c0) < 3)
-        // {
-        //     printf("No data available\n");
-        //     continue;
-        // }
-
-        // read data from i2c_pi.py
-        // i2c_read_timeout_us(i2c0, 0x0703, incoming_data, 3, false, 50000);
-        // i2c_read_raw_blocking(i2c0, incoming_data, 3);
-        int read_error = i2c_read_blocking(i2c0, 0x08, incoming_data, 3, true);
-        gpio_put(LED_PIN, 1);
-
-        if (incoming_data[0] == 0x00)
-        {
-            printf("No data received %d\n", read_error);
-            continue;
-        }
-
-        // sleep_ms(250);
-
-        // process data
-        for (int i = 0; i < 3; i++)
-        {
-            printf("Received data %d: 0x%02X\n ", i, incoming_data[i]);
-        }
-
-        // send data to pi
-        int write_error = i2c_write_timeout_us(i2c0, 0x08, outgoing_data, 3, false, 50000);
-        printf("Write error: %d\n", write_error);
-        // i2c_write_raw_blocking(i2c0, outgoing_data, 3);
-        // i2c_write_blocking(i2c0, 0x08, outgoing_data, 3, true);
-        gpio_put(LED_PIN, 0);
-        // sleep_ms(250);
-    }
+        ;
 
     return 0;
 }

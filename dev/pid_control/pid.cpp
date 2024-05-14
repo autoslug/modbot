413b6576f0cbab8f2182a062dd025515ea65aa1f
brendanRose1 <57075608+brendanRose1@users.noreply.github.com>
2024-05-14 15:56:16 -0700

    commit
    i forgot to commit this, i think it finalizes changes
    -still need to add integral windup correction

--- dev/pid_control/pid.cpp
+++ dev/pid_control/pid.cpp
@@ -5,33 +5,61 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-float calculate(float input, float timeStep);
-void updateIntegralDerivative();
+#include "quadrature_encoder.cpp"
+
+void calculate();
+void updateErrorIntegralDerivative();
 class pid { //pid loop
   public:
-    pid(float P, float I, float D) {
+    pid(float P, float I, float D, Encoder encoder, int timestep = 100, int substep = 10) {
         this->P = P;
         this->I = I;
         this->D = D;
+        this->encoder = encoder;
+        this->timestep = timestep //timestep for updating output
+        this->substep = this->timestep / (float)substep // how many times to update integral/derivative per timestep
+        
     }
 
-    setup(){ // PICO SPECIFIC: create repeating timer, setup callbacks
+    void setup(){ // PICO SPECIFIC: create repeating timer, setup callbacks
         this->timer = repeating_timer_t timer;
-        add_repeating_timer_ms(100, calculate, NULL, timer);
-        add_repeating_timer_ms(10, updateIntegralDerivative, NULL, timer);
+        add_repeating_timer_ms(this->timestep, calculate, NULL, timer);
+        add_repeating_timer_ms(10, updateErrorIntegralDerivative, NULL, timer);
     }
 
+    void updateTarget(float target){
+      //Idea; pid loop is autonomous and will automatically do whatever due to repeating timers
+      //if we just update the target with a function call we might be okay
+      this->target = target;
+    }
+
+    float output(){
+      return this->output;
+    }
   private:
-    
-    void calculate(float input, float timeStep){
-        this->output = this->P * input + this-> I * this -> Integral + this-> D * this -> Derivative;
+    //privated functions for pid function
+    void calculate(){
+        this->output = this->P * this->target + this->I * this->Integral + this->D * this->Derivative;
+    }
+
+    void updateErrorIntegralDerivative(){
+      error = this->encoder.getPosition()-this->target; //error term
+      this->Integral += error * this->substep; //left riemann sum at fixed substep
+      this->Derivative = (this->encoder.getPosition()-this->output)/2; //estimate of derivatvie at fixed substep
     }
 
     float P;
     float I;
     float D;
-    repeating_timer_t timer = {};
+
+    float target = 0;
+    float output = 0;
+    float input = 0;
+
     float Integral = 0;
     float Derivative = 0;
-    float output = 0;
+    repeating_timer_t timer = {};
+    int timestep;
+    int substep;
+    Encoder encoder;
 };

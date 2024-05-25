9fd815bfb0edad804cec20faf5b4deb44bb04a5b
Mignot Mesele <mmesele@ucsc.edu>
2024-05-25 13:10:39 -0700

    save

--- dev/socket_test/client.py
+++ dev/socket_test/client.py
@@ -1,25 +1,22 @@
 import socket
 import json
 from Controller import *
+import sys
 
-
-#so far this has been tested to send array of inputs to server but haven't tested with controller yet.
-#it sends to server and receives a response message, "received request accepted", until stopping connection
-#need to test with controller to see if server has anything besides an array of zeros.
-#need to add signal handling for control-c so it exits with a zero status code.
-
-HOST = "127.0.0.1" 
-PORT = 65432
-
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.connect((HOST, PORT))
-    joy = Controller()
-    while True:
-        try:
-            message = json.dumps(joy.read())
-            s.sendall(message.encode())
-            data = s.recv(1024)
-            print(f"Received {data.decode()!r}")
-        except Exception as e:
-            print("error!", e)
-            break
+HOST = "Change to correct IP" #needs to change depending on what ip the server prints
+PORT = 5050
+try:
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.connect((HOST, PORT))
+        joy = Controller()
+        while True:
+            try:
+                message = json.dumps(joy.read())
+                s.sendall(message.encode())
+                data = s.recv(1024)
+                print(f"Received {data.decode()!r}")
+            except Exception as e:
+                print("error!", e)
+                break
+except KeyboardInterrupt:
+    sys.exit(0)
--- dev/socket_test/server.py
+++ dev/socket_test/server.py
@@ -4,22 +4,30 @@ import json
 #server accepts request and prints out the array of controller inputs but haven't tested with controller yet.
 #it sends back "accepted request" if there's data that's been sent else request DENIED.
 
-HOST = "127.0.0.1" #Standard loopback interface address(localhost)
-PORT = 65432 #port to listen on (non-privileged ports are > 1023)
+HOST = socket.gethostbyname(socket.gethostname())
+PORT = 5050 #port to listen on (non-privileged ports are > 1023)
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    print(HOST)
     s.bind((HOST, PORT))
+    print("------------[STARTING SERVER]------------\nserver is listening....")
     s.listen()
     conn, addr = s.accept()
-    with conn:
-        print(f"Connected by {addr}")
-        while True:
-            data = conn.recv(1024)
-            if not data:
-                message = "request DENIED >:(!!"
+    print("----------[ACCEPTED CONNECTION]----------")
+    try:
+        with conn:
+            print(f"Connected by {addr}")
+            while True:
+                data = conn.recv(1024)
+                if not data:
+                    message = "request DENIED >:(!!"
+                    conn.sendall(message.encode())
+                    break
+                received_data = json.loads(data.decode())
+                message = "Accepted request"
+                print(received_data)
                 conn.sendall(message.encode())
-                break
-            received_data = json.loads(data.decode())
-            message = "Accepted request"
-            print(received_data)
-            conn.sendall(message.encode())
+    except ConnectionResetError:
+        print("----------[CLOSING CONNECTION]----------")
+        conn.close()
+        print("----------[SERVER CLOSED GOODBYE!]----------")
